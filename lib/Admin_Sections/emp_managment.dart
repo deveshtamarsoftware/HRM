79ece3b43c21705ecf687c3935299de2075bddbc
HarshSinghal379 <harsh.singhal@tamarsoftware.in>
2025-08-22 10:04:41 +0530

    frontend of flutter

--- lib/Admin_Sections/emp_managment.dart
+++ lib/Admin_Sections/emp_managment.dart
@@ -20,7 +20,7 @@ class _EmployeeFormState extends State<EmployeeForm> {
   final _phoneController = TextEditingController();
   final _firstNameController = TextEditingController();
   final _lastNameController = TextEditingController();
-// droghh
+// droghhf
   // Dropdown + Date
   String _selectedGender = 'Male';
   String _selectedMaritalStatus = 'Single';
